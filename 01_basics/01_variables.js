f84c4c65cdd7f59e8e33746bafc3bb7e7e141d58
Vikash Rawat <168196937+VikashRawat07@users.noreply.github.com>
2024-06-08 14:52:26 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountEmail= "vikash@google"
+var accountPassword = "12345"
+accountCity ="Jaipur"
+var accountState;
+//accountID =2//not allowed
+accountEmail = "djdj.comj"
+accountPassword = "34576890"
+accountCity = "Bengaluru"
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+console.log(accountID);
+/*
+Prefer not to use var because of blocked scope and functional scope 
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"// treat all JS as newer version
+
+// alert (3+3) // we are using nodejs not browser
+console.log(3+
+    3) // code readability should be high
+
+let name = "Bijdj"
+let age =2
+let isLoggedIn= false
+//number=> 2 to power 53
+//bigint
+//string=> ""
+//boolean=> true/false
+//null=> standalone value
+//undefined=> 
+//symbol => unique
+//object
+console.log(typeof null)// object
+console.log(typeof undefined) // undefined
