306838f0c1be048aff124756709253c379df9759
Vikash Rawat <168196937+VikashRawat07@users.noreply.github.com>
2024-06-12 06:48:31 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -9,11 +9,34 @@
 //true => 1; false=> 0
 let isLoggedIn= 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 //1 => true; 0=> false
 //""=> false
 //"hitesh"=> true
 let someNumber =33
 let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+// Operations
+let value= 3
+let negvalue= -value
+//console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2); power
+// console.log(2/2);
+// console.log(2%2);
+let str1= "hello"
+let str2= " hitesh"
+let str3= str1+ str2
+console.log(str3)
+console.log("1" + 2)
+console.log(1+ "2")
+console.log("1"+ "2")
+console.log(+true)
+console.log(+"")
+let num1, num2, num3
+num1= num2= num3= 2+2
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
